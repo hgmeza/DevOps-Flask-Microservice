e69ed0e5b102e8c1c07b25a422dae0bc7a888c0c
Hugo Meza <hgmeza114@gmail.com>
2020-06-22 23:02:34 -0400

    add docker commands to run the project

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build . -t "hgmeza/devops-flask-microservices"
 # Step 2: 
 # List docker images
-
+docker images ls
 # Step 3: 
 # Run flask app
+docker run  -p 127.0.0.1:8000:80 --name "devops-flask-microservices" "hgmeza/devops-flask-microservices"
